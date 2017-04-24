0bbee5f3bf8c2601cd51e81e10e02532fec58a98
Tiger Fok <tiger.indzz@gmail.com>
2017-04-24 12:56:09 +0800

    Fixed delete operation

--- src/stubs/views/edit.blade.php
+++ src/stubs/views/edit.blade.php
@@ -10,3 +10,15 @@
 <button type="submit" name="form_submit" class="btn-success btn">{{ trans('backend.button.save') }}</button>
 <button type="button" name="form_delete" class="btn-danger btn pull-right">{{ trans('backend.button.delete') }}</button>
 @endpush
+
+@push('js')
+<script>
+    $().ready(function () {
+        $('button[name=form_delete]').on('click', function () {
+            $form = $(this).closest('form');
+            $form.find('input[name=_method]').val('delete');
+            $form.submit();
+        });
+    });
+</script>
+@endpush
