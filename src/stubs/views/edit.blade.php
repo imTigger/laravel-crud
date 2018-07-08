f9adb7519921ebd347b876662604917edeaa9b6d
Tiger Fok <tiger@tiger-workshop.com>
2018-07-09 04:45:21 +0800

    Cleanup

--- src/stubs/views/edit.blade.php
+++ src/stubs/views/edit.blade.php
@@ -9,15 +9,3 @@
 @push('actions')
 <button type="submit" class="btn-success btn">{{ trans('backend.button.save') }}</button>
 @endpush
-
-@push('js')
-<script>
-    $().ready(function () {
-        $('button[name=form_delete]').on('click', function () {
-            $form = $(this).closest('form');
-            $form.find('input[name=_method]').val('delete');
-            $form.submit();
-        });
-    });
-</script>
-@endpush
