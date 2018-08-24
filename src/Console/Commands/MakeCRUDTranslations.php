9655b6cf493558274caecd16b3bb946530e620c5
Tiger Fok <tiger@tiger-workshop.com>
2018-08-25 02:22:26 +0800

    Added two new utility commands

---
+++ src/Console/Commands/MakeCRUDTranslations.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class MakeCRUDTranslations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:crud:trans';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate translations strings from key for Laravel Translation Manager';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $rows = DB::table('ltm_translations')->where('status', '=', 0)->whereNull('value')->get();
+
+        foreach ($rows as $row) {
+            $value = substr($row->key, strrpos($row->key, '.') + 1);
+            $value = ucwords(str_replace("_", " ", $value));
+            $value = str_replace(['Id'], ['ID'], $value);
+
+            DB::table('ltm_translations')
+                ->where('id', $row->id)
+                ->update(['value' => $value]);
+        }
+
+        $this->info(sizeof($rows) . ' rows updated');
+    }
+}
